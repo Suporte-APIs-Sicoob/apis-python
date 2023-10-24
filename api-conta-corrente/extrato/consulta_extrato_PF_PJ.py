bbf7ee8def2c76a3be1b09779fc6744e2a576b13
Suporte as API's <142435956+Suporte-APIs-Sicoob@users.noreply.github.com>
2023-10-24 11:35:13 -0300

    Consulta de extrato de acordo com mes e ano

--- api-conta-corrente/extrato/consulta_extrato_PF_PJ.py
+++ api-conta-corrente/extrato/consulta_extrato_PF_PJ.py
@@ -1,9 +1,9 @@
 import requests
-from Python.api_conta_corrente.requisicao_token import token
+from requisicao_token import token
 
 extrato = "06/2022"
 numeroContaCorrente = "123456"
-client_id = "68d0d27a-3ffa-41bf-93a5-25cbff71500f"
+client_id = "1234567a-1ab2-12ab-12a3-12abcd71234a"
 
 payload = {}
 headers = {
@@ -14,7 +14,3 @@ headers = {
 }
 
 response = requests.request("GET", f"https://api.sicoob.com.br/conta-corrente/extrato/{extrato}?numeroContaCorrente={numeroContaCorrente}", headers=headers, data=payload)
-
-print(response.text)
-
-
